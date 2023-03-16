3c862c317160fa6740877159d20a67446ad1cb7f
Nilton <niltonfontes1999@gmail.com>
2023-03-16 16:17:17 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { Link } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const HomePage = () => {
   return (
     <Layout pageTitle="Home Page" key={"home-page"}>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Static Image Home Page"
+        src="../images/king-bob2.webp"
+      />
     </Layout>
   );
 };
